fc534365579053369b09bd92acb9d8c9e7a5417a
MUKUL BANSAL <curlvector747@gmail.com>
2022-01-22 14:18:51 +0530

    Create Equal Card Game.cpp

---
+++ Equal Card Game.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	int k;
+	cin>>k;
+	
+	cout<<52%k<<endl;
+} 
+		
+	return 0;
+	
+}
