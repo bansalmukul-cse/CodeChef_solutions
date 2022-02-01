f5a0a4990609dc2ca18ec215177a861e25c86bf2
MUKUL BANSAL <curlvector747@gmail.com>
2022-02-01 12:45:50 +0530

    Create Average of Three .cpp

---
+++ Average of Three .cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	int x;
+	cin>>x;
+	
+	cout<<x-1<<" "<<x<<" "<<x+1<<endl;
+	
+
+	
+	
+} 
+		
+	return 0;
+	
+}
