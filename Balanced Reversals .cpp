80d754e7d3776447a6c4abd32fe43c9bcff09cd0
MUKUL BANSAL <curlvector747@gmail.com>
2022-03-17 00:40:11 +0530

    Create Balanced Reversals .cpp

---
+++ Balanced Reversals .cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h> // Mukul Bansal
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		string s;
+		cin>>s;
+		sort(s.begin(),s.end());
+		cout<<s;
+		cout<<endl;
+;		
+		 
+    
+} 
+		
+	return 0;
+	
+}
