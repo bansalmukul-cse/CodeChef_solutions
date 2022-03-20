ad166d689552c5762b567a3eefbf911340929273
MUKUL BANSAL <curlvector747@gmail.com>
2022-03-20 09:16:48 +0530

    Create Credit score .cpp

---
+++ Credit score .cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	
+		int x;
+		cin>>x;
+		if(x>=750){
+			cout<<"YES"<<endl;
+		}
+		else{
+			cout<<"NO"<<endl;
+		}
+		
+	return 0;
+	
+}
