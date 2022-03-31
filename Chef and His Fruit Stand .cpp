b04eb5cdeaa327263168f66de22afeeeca4f7433
MUKUL BANSAL <curlvector747@gmail.com>
2022-03-31 12:49:02 +0530

    Create Chef and His Fruit Stand .cpp

---
+++ Chef and His Fruit Stand .cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	int x,y;
+	cin>>x>>y;
+	int bana = x/2;
+	
+	if(y>=bana)
+		cout<<bana<<endl;
+	else
+		cout<<y<<endl;
+	
+	
+	
+} 
+		
+	return 0;
+	
+}
