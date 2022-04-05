413f764895d5161869f012803907c83412680a03
MUKUL BANSAL <curlvector747@gmail.com>
2022-04-05 11:16:11 +0530

    Create Email Reminders.cpp

---
+++ Email Reminders.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	
+	int n,u;
+	cin>>n>>u;
+	int res;
+	res = n-u;
+	cout<<res<<endl;
+	
+		
+    return 0;
+	
+}
