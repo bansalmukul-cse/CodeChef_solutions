cddcd54b188d4ff50dd7bd4733bcfabf402dd9ce
MUKUL BANSAL <curlvector747@gmail.com>
2022-05-17 00:08:54 +0530

    Create How many unattempted problems .cpp

---
+++ How many unattempted problems .cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y;
+	cin>>x>>y;
+	int res;
+	res = x-y;
+	cout<<res;
+		
+	return 0;
+	
+}
