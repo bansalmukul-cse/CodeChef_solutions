9021cb3a365debbb11f2796247380ffa9b1869c6
MUKUL BANSAL <curlvector747@gmail.com>
2022-06-14 10:13:38 +0530

    Create Chairs Requirement.cpp

---
+++ Chairs Requirement.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    if(x>=y){
+	        cout<<x-y<<endl;
+	    }
+	    else{
+	        cout<<0<<endl;
+	    }
+		
+} 
+		
+	return 0;
+	
+}
