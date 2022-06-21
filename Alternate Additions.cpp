c7f47a9d1c3de4ecc0d1a6e340421245384a9fdf
MUKUL BANSAL <curlvector747@gmail.com>
2022-06-21 22:16:30 +0530

    Create Alternate Additions.cpp

---
+++ Alternate Additions.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    if((b-a)%3==2){
+	        cout<<"NO"<<endl;
+	    }
+	    else{
+	        cout<<"YES"<<endl;
+	    }
+		
+} 
+		
+	return 0;
+	
+}
