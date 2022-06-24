37f402f4e12d571c4dbb2fbb78c150de7dfa4889
MUKUL BANSAL <curlvector747@gmail.com>
2022-06-24 22:50:08 +0530

    Create Determine the Score.cpp

---
+++ Determine the Score.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,n;
+	    cin>>x>>n;
+	    int val = x/10;
+	    
+	    int res = val*n;
+	    
+	    cout<<res<<endl;
+	    
+		
+} 
+		
+	return 0;
+	
+}
