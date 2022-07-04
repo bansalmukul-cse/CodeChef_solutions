c8d70d70b2f3c2986ddc330d0bc9ce955b4248ad
MUKUL BANSAL <curlvector747@gmail.com>
2022-07-04 12:25:27 +0530

    Create Air Hockey.cpp

---
+++ Air Hockey.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    
+	    if(a>b){
+	        cout<<7-a<<endl;
+	    }
+	    else{
+	        cout<<7-b<<endl;
+	    }
+		
+} 
+		
+	return 0;
+	
+}
