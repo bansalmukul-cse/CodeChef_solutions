9b3fe2587b5e328a6f869f76b3f1d127ebc193eb
MUKUL BANSAL <curlvector747@gmail.com>
2022-07-12 12:02:34 +0530

    Create Best of Two.cpp

---
+++ Best of Two.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    
+	    if(x>y){
+	        cout<<x<<endl;
+	    }
+	    else{
+	        cout<<y<<endl;
+	    }
+		
+} 
+		
+	return 0;
+	
+}
