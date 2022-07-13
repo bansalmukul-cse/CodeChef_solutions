7a62ca247ef6bfd644c84c172f5540ed30b03b00
MUKUL BANSAL <curlvector747@gmail.com>
2022-07-13 08:45:59 +0530

    Create Change Row and Column Both.cpp

---
+++ Change Row and Column Both.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int a ,b,c,d;
+		cin>>a>>b>>c>>d;
+		
+		if(a==c || b==d){
+		    cout<<2<<endl;
+		}
+		else{
+		     cout<<1<<endl;
+		}
+} 
+		
+	return 0;
+	
+}
