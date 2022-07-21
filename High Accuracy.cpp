91563eac392e4d90349c9bd24443f63952d6d2e8
MUKUL BANSAL <curlvector747@gmail.com>
2022-07-21 12:45:38 +0530

    Create High Accuracy.cpp

---
+++ High Accuracy.cpp
@@ -0,0 +1,37 @@
+
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int x;
+		cin>>x;
+		
+		
+		if(x%3==0){
+		    cout<<0<<endl;
+		}
+		else{
+		    if(x%3==1){
+		        cout<<2<<endl;
+		    }
+		    else{
+		        cout<<1<<endl;
+		    }
+		}
+		
+	
+	
+} 
+		
+	return 0;
+	
+}
