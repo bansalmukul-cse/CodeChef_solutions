0f8b78fb77be8e95cecf9b36d851fbfae8a0cc6e
MUKUL BANSAL <curlvector747@gmail.com>
2022-07-22 14:37:07 +0530

    Create Counting Words.cpp

---
+++ Counting Words.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>>n>>m;
+	    
+	    int res = n*m;
+	    cout<<res<<endl;
+		
+} 
+		
+	return 0;
+	
+}
