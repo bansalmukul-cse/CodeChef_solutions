bb26c9b2a9afea2d7884d30e46a99d050c90d366
MUKUL BANSAL <curlvector747@gmail.com>
2022-07-29 15:45:28 +0530

    Create Chess Time.cpp

---
+++ Chess Time.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    int min = n*60;
+	    
+	    int res = min / 20;
+	    
+	    cout<<res<<endl;
+		
+} 
+		
+	return 0;
+	
+}
