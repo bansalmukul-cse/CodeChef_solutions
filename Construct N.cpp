9232a225db2ee1390f5250fe4b09d3e46bec40d6
MUKUL BANSAL <curlvector747@gmail.com>
2022-07-30 18:54:44 +0530

    Create Construct N.cpp

---
+++ Construct N.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    if(n==1 || n==3 || n==5){
+	        cout<<"NO"<<endl;
+	    }
+	    else{
+	        cout<<"YES"<<endl;
+	    }
+		
+} 
+		
+	return 0;
+	
+}
