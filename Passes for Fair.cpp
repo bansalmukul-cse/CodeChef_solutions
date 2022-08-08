fcbc398c79f0d26a3605a0b83b43882998f7ae04
MUKUL BANSAL <curlvector747@gmail.com>
2022-08-08 23:48:36 +0530

    Create Passes for Fair.cpp

---
+++ Passes for Fair.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,k;
+	    cin>>n>>k;
+	    
+	    
+	    if(n==k){
+	        cout<<"NO"<<endl;
+	    }
+	    else if(n<k){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+		
+} 
+		
+	return 0;
+	
+}
