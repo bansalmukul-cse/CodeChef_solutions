afc1c3799491496e48a89251447a518b51ab8f1b
MUKUL BANSAL <curlvector747@gmail.com>
2022-08-15 22:21:06 +0530

    Create Insurance.cpp

---
+++ Insurance.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    
+	    if(x<y){
+	        cout<<x<<endl;
+	    }
+	    else if(x>y){
+	        cout<<y<<endl;
+	    }
+	    else{
+	         cout<<x<<endl;
+	    }
+		
+} 
+		
+	return 0;
+	
+}
