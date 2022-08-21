e9ee2d9223f47444c08ff9de06d59d2d466fc65c
MUKUL BANSAL <curlvector747@gmail.com>
2022-08-22 00:06:05 +0530

    Create Final Population.cpp

---
+++ Final Population.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    
+	    int res = x -(y) +z;
+	    cout<<res<<endl;
+	    
+	    
+	    
+	    
+		
+} 
+		
+	return 0;
+	
+}
