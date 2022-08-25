070c801a62b54ad32ede9d5e5eb1db680c575379
MUKUL BANSAL <curlvector747@gmail.com>
2022-08-25 16:12:44 +0530

    Create Split N.cpp

---
+++ Split N.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	   
+	    
+	     long long int n;
+	     cin >> n;
+	     cout << __builtin_popcountll(n) -1 << endl;
+	    
+	    
+	    
+		
+} 
+		
+	return 0;
+	
+}
