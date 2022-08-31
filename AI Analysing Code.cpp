a8d7288bae6f1d68853e6dde30581e4e67f11863
MUKUL BANSAL <curlvector747@gmail.com>
2022-09-01 00:46:07 +0530

    Create AI Analysing Code.cpp

---
+++ AI Analysing Code.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	    int x;
+	    cin>>x;
+	    
+	    if(x<=1000){
+	        cout<<"yes"<<endl;
+	       
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	   
+		
+	return 0;
+	
+}
