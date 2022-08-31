25b83536d2efa3ae4cfe4dbdeec10d330ad6d672
MUKUL BANSAL <curlvector747@gmail.com>
2022-09-01 00:46:39 +0530

    Create Air Conditioner Temperature.cpp

---
+++ Air Conditioner Temperature.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a, b, c;
+	    cin>>a>>b>>c;
+	    
+	    if(b>=a && b>=c ){
+	        cout<<"yes"<<endl;
+	    }
+	    else{
+	        cout<<"No"<<endl;
+	    }
+	    
+	    
+	    
+	    
+		
+} 
+		
+	return 0;
+	
+}
