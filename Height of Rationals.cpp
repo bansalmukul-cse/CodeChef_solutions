f9c3d8d823b9759f05af7bc8cd1b6a542cdaa19f
MUKUL BANSAL <curlvector747@gmail.com>
2022-09-05 13:10:26 +0530

    Create Height of Rationals.cpp

---
+++ Height of Rationals.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	    int a,b;
+	    cin>>a>>b;
+	    
+	    if(a>b){
+	        cout<<a<<endl;
+	    }
+	    else{
+	        cout<<b<<endl;
+	    }
+	    
+	    
+		
+	return 0;
+	
+}
