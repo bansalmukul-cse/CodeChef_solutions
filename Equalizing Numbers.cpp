79b1d6a48a3e85687802947642299093c952fdb1
MUKUL BANSAL <curlvector747@gmail.com>
2022-09-07 16:42:47 +0530

    Create Equalizing Numbers.cpp

---
+++ Equalizing Numbers.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    
+	    int res = abs(a-b);
+	    
+	    if(res%2==0){
+	        cout<<"yes"<<endl;
+	    }
+	    else{
+	        cout<<"no"<<endl;
+	    }
+	    
+	    
+	    
+	    
+		
+} 
+		
+	return 0;
+	
+}
