d8bd7c603fdb99c07a64ac4bb3dac43df8d2395d
MUKUL BANSAL <curlvector747@gmail.com>
2022-09-08 10:33:53 +0530

    Create Candy Distribution.cpp

---
+++ Candy Distribution.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>>n>>m;
+	    
+	    if(n%m==0 && (n/m)%2==0){
+	        cout<<"yes"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	    
+	    
+	    
+		
+} 
+		
+	return 0;
+	
+}
