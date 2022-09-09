6ff6aaa9b24f69f9f363a073b9b416e6927f9f47
MUKUL BANSAL <curlvector747@gmail.com>
2022-09-10 01:19:35 +0530

    Create Easy Permutation.cpp

---
+++ Easy Permutation.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    
+	    int n;
+	    cin>>n;
+	    
+	    for(int i=0;i<n;i++){
+	        cout<<n-i<<" ";
+	    }
+	    cout<<endl;
+	    
+	    
+		
+} 
+		
+	return 0;
+	
+}
