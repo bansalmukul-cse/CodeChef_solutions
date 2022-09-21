fedeacc0ebd1efea0d277dc4bbd487912aefead1
MUKUL BANSAL <curlvector747@gmail.com>
2022-09-21 18:25:15 +0530

    Create Ticket Fine.cpp

---
+++ Ticket Fine.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,p,q;
+	    cin>>x>>p>>q;
+	    
+	    int res = (p-q);
+	    
+	    int val = x *res;
+	    
+	    cout<<val<<endl;
+	    
+	    
+	    
+		
+} 
+		
+	return 0;
+	
+}
