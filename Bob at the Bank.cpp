3752684e7d03045e14167ad945baa05fdd9fd147
MUKUL BANSAL <curlvector747@gmail.com>
2022-09-28 21:17:49 +0530

    Create Bob at the Bank.cpp

---
+++ Bob at the Bank.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    
+	    int w,x,y,z;
+	    cin>>w>>x>>y>>z;
+	    
+	   
+	    cout<<(((x-y)*z)+w)<<endl;
+	    
+	    
+		
+} 
+		
+	return 0;
+	
+}
