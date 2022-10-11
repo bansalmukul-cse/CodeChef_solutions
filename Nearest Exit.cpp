be4797bbd31bb35dfba3e8fcf88ba629d0252352
MUKUL BANSAL <curlvector747@gmail.com>
2022-10-11 20:58:12 +0530

    Create Nearest Exit.cpp

---
+++ Nearest Exit.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    
+	    if(x<=50){
+	        cout<<"LEFT"<<endl;
+	    }
+	    else{
+	        cout<<"Right"<<endl;
+	    }
+	    
+	    
+	    
+	    
+		
+} 
+		
+	return 0;
+	
+}
