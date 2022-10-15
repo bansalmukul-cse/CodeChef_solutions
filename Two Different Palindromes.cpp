9e17d33cc24f47354878c889d4584f1c4586e6d9
MUKUL BANSAL <curlvector747@gmail.com>
2022-10-15 16:52:33 +0530

    Create Two Different Palindromes.cpp

---
+++ Two Different Palindromes.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    
+	    int a,b;
+	    cin>>a>>b;
+	    
+	    if(a%2==1 && b%2==1){
+	        cout<<"No"<<endl;
+	    }
+	    else{
+	        if(a==1 || b==1){
+	        cout<<"NO"<<endl;
+	        }
+	        else{
+	            cout<<"yes"<<endl;
+	        }
+	    
+	    
+	    
+	    }
+	    
+	    
+	    
+		
+} 
+		
+	return 0;
+	
+}
