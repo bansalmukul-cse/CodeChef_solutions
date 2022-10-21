b741634a26fceb055270b5901b9c8308de63b57e
MUKUL BANSAL <curlvector747@gmail.com>
2022-10-21 12:32:45 +0530

    Create Flip the cards.cpp

---
+++ Flip the cards.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    
+	    int n,x;
+	    cin>>n>>x;
+	    
+	    int diff = n-x;
+	    
+	    cout<<min(diff,x)<<endl;
+	    
+	    
+		
+} 
+		
+	return 0;
+	
+}
