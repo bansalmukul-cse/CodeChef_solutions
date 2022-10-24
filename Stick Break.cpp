5dc0d7a183a328af151ba840bac1cbbed7aeeb94
MUKUL BANSAL <curlvector747@gmail.com>
2022-10-24 13:04:09 +0530

    Create Stick Break.cpp

---
+++ Stick Break.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,k;
+	    cin>>n>>k;
+	    
+	    if(n%k){
+	        cout<<1<<endl;
+	    }
+	    else{
+	        cout<<0<<endl;
+	    }
+	    
+	    
+	    
+		
+} 
+		
+	return 0;
+	
+}
