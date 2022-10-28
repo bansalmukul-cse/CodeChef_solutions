59f771a65fb8a9a8369dcff625cc36b9d95945b1
MUKUL BANSAL <curlvector747@gmail.com>
2022-10-28 20:38:33 +0530

    Create Single Operation Part 2.cpp

---
+++ Single Operation Part 2.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,i,c=0;
+	    cin>>n;
+	    string s;
+	    cin>>s;
+	    
+	    for(i=0;i<n;i++){
+	        c++;
+	        if(s[i+1]=='1'){
+	            break;
+	        }
+	    }
+	    cout<<c<<endl;
+	    
+	    
+	    
+		
+} 
+		
+	return 0;
+	
+}
