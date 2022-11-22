6e71e6f30f37c050920e7cdf37953a4d83247b22
MUKUL BANSAL <curlvector747@gmail.com>
2022-11-22 22:57:26 +0530

    Create Important Pages on CodeChef.cpp

---
+++ Important Pages on CodeChef.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h> // bansal_mukul
+#include <cmath>
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	
+	int a,b;
+	cin>>a>>b;
+	if(a==1 && b==1){
+		cout<<"https://discuss.codechef.com"<<endl;
+	}
+	else if(a==0){
+		cout<<"https://www.codechef.com/practice"<<endl;
+	}
+	else{
+		cout<<"https://www.codechef.com/contests"<<endl;
+	}
+		
+	return 0;
+	
+}
