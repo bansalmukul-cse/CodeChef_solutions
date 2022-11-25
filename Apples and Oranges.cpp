b20d0d6efa21de83b618f57b1fd8a5a03ff4470a
MUKUL BANSAL <curlvector747@gmail.com>
2022-11-25 22:37:37 +0530

    Create Apples and Oranges.cpp

---
+++ Apples and Oranges.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x,a,b;
+	cin>>x>>a>>b;
+	if(x>=(a+b)){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
